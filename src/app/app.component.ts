0cc2b4e214aa6c10dcc8f087cf530079144b0db6
edwargarzon <160767624+edwargarzon@users.noreply.github.com>
2025-07-07 10:16:34 -0500

    Add app.component.ts

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: '<h1>Hola Mundo</h1>',
+  styles: []
+})
+export class AppComponent {
+  title = 'demo-ia-para-todos-mcp-angular';
+}
